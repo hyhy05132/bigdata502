1e27c960e271ddaa42ab5b5f369f49d25ace9198
ssar <ssar@nate.com>
2019-09-20 13:55:21 +0900

    test

---
+++ Animal.java
@@ -0,0 +1,21 @@
+
+public class Animal {
+	public void play() {
+		System.out.println("港港");
+	}
+
+	public void play1() {
+		System.out.println("具克");
+	}
+
+	public void play2() {
+		System.out.println("卤");
+	}
+
+	public static void main(String[] args) {
+		Animal a = new Animal();
+		a.play();
+		a.play1();
+		a.play2();
+	}
+}
---
+++ Animal2.java
@@ -0,0 +1,12 @@
+
+public class Animal2 {
+	public void play() {
+		System.out.println("¸Û¸Û");
+	}
+
+	public static void main(String[] args) {
+		Animal2 a = new Animal2();
+		a.play();
+	}
+
+}
